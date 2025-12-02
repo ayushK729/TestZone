68a9e9c2a48b1a3086a1af389e0791452cf393f8
Ayush <ayushkumar@work.com>
2025-12-02 20:47:42 +0530

    Added isPrime function

--- new.cpp
+++ new.cpp
@@ -1,10 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
+bool isPrime(int n)
+{
+    bool f=true;
+    for(int i=2;i<n;i++)
+        if(n%i==0)
+        {
+            f=false;
+            break;
+        }
+    return f;
+}
 int main()
 {
     int n;
     cout<<"Input a number of choice:";
     cin>>n;
+    cout<<"Is "<<n<<" a prime number --> "<<isPrime(n)<<endl;
     cout<<"Hello World!";
     return 0;
 }
